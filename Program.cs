5254c0c0b26f26a30ee40bf0b6d4e5e1d78db723
bittiboop <sstrelkova15@gmail.com>
2024-11-12 20:32:04 +0200

    added ex-1

--- Program.cs
+++ Program.cs
@@ -4,8 +4,10 @@
     {
         static void Main()
         {
+            Console.WriteLine("It's easy to win forgiveness for being wrong; ");
+            Console.WriteLine("being right is what gets you into real trouble.");
+            Console.WriteLine("Bjarne Stroustrup");
 
-            
         }
     }
 }
