afb0a413ab035d830086c7e857f33174a9571503
bittiboop <sstrelkova15@gmail.com>
2024-11-23 14:46:48 +0200

    added ex-3

--- Program.cs
+++ Program.cs
@@ -6,13 +6,17 @@ namespace _1C_
     {
         static void Main()
         {
-            Console.WriteLine("Enter the number: ");
-            int num = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the rate: ");
-            int rate = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"Rate {rate} of this {num} number: ");
-            Console.WriteLine(num / rate);
+            Console.WriteLine("Enter the first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the third number: ");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the fourth number: ");
+            int num4 = Convert.ToInt32(Console.ReadLine());
 
+            Console.WriteLine("We get number: ");
+            Console.WriteLine(num1 * 1000 + num2 * 100 + num3 * 10 + num4);
         }
     }
 }
