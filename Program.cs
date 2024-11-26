f4d87d39cb404a24207f4a257fb30d1a4cce00fa
bittiboop <sstrelkova15@gmail.com>
2024-11-26 19:44:15 +0200

    added ex4 from hw

--- Program.cs
+++ Program.cs
@@ -4,8 +4,33 @@
     {
         static void Main()
         {
+            Console.WriteLine("Enter 6-digit number: ");
+            string number = Console.ReadLine();
 
+            if (number.Length != 6)
+            {
+                Console.WriteLine("Invalid number");
+                return;
+            }
+            char[] digits = number.ToCharArray();
+            Console.WriteLine("Enter bit numbers to replace: ");
+            if(!int.TryParse(Console.ReadLine(), out int bit1) || !int.TryParse(Console.ReadLine(), out int bit2))
+            {
+                Console.WriteLine("Invalid bit number");
+                return;
+            }
             
+            Console.WriteLine("Enter new bit: ");
+            if (!int.TryParse(Console.ReadLine(), out int newBit))
+            {
+                Console.WriteLine("Invalid new bit");
+                return;
+            }
+
+            char newDigit = Console.ReadKey().KeyChar;
+            digits[bit1 - 1] = newDigit;
+            string newNumber = new string(digits);
+            Console.WriteLine($"\nNew number: {newNumber}");
         }
     }
 }
